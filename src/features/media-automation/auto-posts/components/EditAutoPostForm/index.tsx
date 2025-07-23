b84ff9980a7788d0037cd49f034b59b83a33440b
ItsMeNozo <dungtuyetngoc@gmail.com>
2025-07-23 12:33:52 +0700

    feat: refactor mock data structure and introduce Post type with status enum

--- src/features/media-automation/auto-posts/components/EditAutoPostForm/index.tsx
+++ src/features/media-automation/auto-posts/components/EditAutoPostForm/index.tsx
@@ -3,12 +3,15 @@ import InlineErrorMessage from '@/components/InlineErrorMessage';
 import Loading from '@/components/loading/Loading';
 import { useConfirmationDialog } from '@/hooks/useConfirmationDialog';
 import { useNumericParam } from '@/hooks/useNumericParam';
-import { Box, Button, Tooltip, Typography } from '@mui/material';
+import { Box, Button, Typography } from '@mui/material';
 import { useQueryClient } from '@tanstack/react-query';
 import { ErrorMessage, Form, Formik, FormikHelpers } from 'formik';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { useMemo } from 'react';
 import { LuTrash2 } from 'react-icons/lu';
+import { PiStarFourFill } from 'react-icons/pi';
+import { TbGridDots } from 'react-icons/tb';
+import { useNavigate } from 'react-router-dom';
 import * as Yup from 'yup';
 import { MAX_IMAGE_COUNT } from '../../constants';
 import { useDeleteAutoPost } from '../../hooks/useDeleteAutoPost';
@@ -17,11 +20,8 @@ import { useGetAutoPostDetails } from '../../hooks/useGetAutoPostDetails';
 import { AutoPostFormValues, ImageState } from '../../types';
 import PostContentEditor from './PostContentEditor';
 import PostImagesEditor from './PostImagesEditor';
-import PostScheduleEditor from './PostScheduleEditor';
 import { FacebookPostPreview } from './PostPreviewer';
-import { TbGridDots } from 'react-icons/tb';
-import { useNavigate } from 'react-router-dom';
-import { PiStarFourFill } from 'react-icons/pi';
+import PostScheduleEditor from './PostScheduleEditor';
 
 // import { Link, Element } from "react-scroll";
 
@@ -101,7 +101,7 @@ const EditAutoPostForm = () => {
 
   if (!postToEdit) {
     return (
-      <Box className="flex justify-center items-center h-full">
+      <Box className="flex h-full items-center justify-center">
         <Typography variant="h6" className="text-gray-600">
           Post not found or has been deleted
         </Typography>
@@ -122,31 +122,35 @@ const EditAutoPostForm = () => {
     >
       {({ values, setFieldValue, errors, touched, isSubmitting, dirty }) => {
         return (
-          <Form className="flex flex-col bg-[#F2F4F7] rounded-t-3xl w-full h-[calc(100vh-4rem)]">
-            <div className="flex items-center bg-white px-4 py-2 border-mountain-200 border-b-1 rounded-t-3xl w-full h-16 shrink-0">
-              <div className="flex justify-between items-center w-full">
-                <div className='flex space-x-4'>
-                  <div className='flex items-center space-x-4'>
-                    <div className='flex items-center space-x-2 bg-indigo-100 p-2 px-4 border border-mountain-200 rounded-full cursor-pointer'>
+          <Form className="flex h-[calc(100vh-4rem)] w-full flex-col rounded-t-3xl bg-[#F2F4F7]">
+            <div className="border-mountain-200 flex h-16 w-full shrink-0 items-center rounded-t-3xl border-b-1 bg-white px-4 py-2">
+              <div className="flex w-full items-center justify-between">
+                <div className="flex space-x-4">
+                  <div className="flex items-center space-x-4">
+                    <div className="border-mountain-200 flex cursor-pointer items-center space-x-2 rounded-full border bg-indigo-100 p-2 px-4">
                       <span>Project Posts</span>
                       <TbGridDots />
                     </div>
-                    <button type='button' onClick={handleAddPost} className='flex items-center space-x-2 hover:bg-mountain-50 p-2 border border-mountain-200 rounded-lg cursor-pointer'>
-                      <PiStarFourFill className='size-4 text-purple-600' />
+                    <button
+                      type="button"
+                      onClick={handleAddPost}
+                      className="hover:bg-mountain-50 border-mountain-200 flex cursor-pointer items-center space-x-2 rounded-lg border p-2"
+                    >
+                      <PiStarFourFill className="size-4 text-purple-600" />
                       <span>Generate Post</span>
                     </button>
-                    <div className='flex items-center px-4 border-mountain-200 border-l-1'>
-                      <div className='flex items-center space-x-2'>
-                        <div className='flex items-center bg-white p-2 border border-mountain-200 rounded-lg cursor-pointer'>
-                          <ChevronLeft className='size-4' />
+                    <div className="border-mountain-200 flex items-center border-l-1 px-4">
+                      <div className="flex items-center space-x-2">
+                        <div className="border-mountain-200 flex cursor-pointer items-center rounded-lg border bg-white p-2">
+                          <ChevronLeft className="size-4" />
                           <span>Prev</span>
                         </div>
-                        <div className='flex items-center bg-white p-2 border border-mountain-200 rounded-lg cursor-pointer'>
+                        <div className="border-mountain-200 flex cursor-pointer items-center rounded-lg border bg-white p-2">
                           <span>Post Number 1</span>
                         </div>
-                        <div className='flex items-center bg-white p-2 border border-mountain-200 rounded-lg cursor-pointer'>
+                        <div className="border-mountain-200 flex cursor-pointer items-center rounded-lg border bg-white p-2">
                           <span>Next</span>
-                          <ChevronRight className='size-4' />
+                          <ChevronRight className="size-4" />
                         </div>
                       </div>
                     </div>
@@ -163,7 +167,7 @@ const EditAutoPostForm = () => {
                   <Button
                     type="button"
                     onClick={() => openDialog(postId!)}
-                    className="flex items-center space-x-2 bg-mountain-100 hover:bg-mountain-50 p-2 border border-mountain-200 rounded-lg"
+                    className="bg-mountain-100 hover:bg-mountain-50 border-mountain-200 flex items-center space-x-2 rounded-lg border p-2"
                   >
                     <LuTrash2 className="size-4" />
                     <div>Delete</div>
@@ -171,10 +175,10 @@ const EditAutoPostForm = () => {
                 </div>
               </div>
             </div>
-            <Box className='flex w-full h-screen min-h-0'>
-              <Box className="flex flex-col space-y-8 px-2 border-mountain-200 border-r-1 w-lg min-h-0 overflow-x-hidden overflow-y-auto custom-scrollbar">
+            <Box className="flex h-screen min-h-0 w-full">
+              <Box className="border-mountain-200 custom-scrollbar flex min-h-0 w-lg flex-col space-y-8 overflow-x-hidden overflow-y-auto border-r-1 px-2">
                 <Box className="flex flex-col space-y-4">
-                  <Typography className="flex items-center space-x-2 py-2 border-mountain-200 border-b-1 text-indigo-900">
+                  <Typography className="border-mountain-200 flex items-center space-x-2 border-b-1 py-2 text-indigo-900">
                     <span className="mr-2">🖊️</span>Post Content
                   </Typography>
                   <PostContentEditor
@@ -188,7 +192,7 @@ const EditAutoPostForm = () => {
                   </ErrorMessage>
                 </Box>
                 <Box className="flex flex-col space-y-4">
-                  <div className="flex items-center space-x-2 py-2 border-mountain-200 border-b-1 text-indigo-900">
+                  <div className="border-mountain-200 flex items-center space-x-2 border-b-1 py-2 text-indigo-900">
                     <p>🖼️</p>
                     <p>Post Images</p>
                   </div>
@@ -204,7 +208,7 @@ const EditAutoPostForm = () => {
                   </ErrorMessage>
                 </Box>
                 <Box className="flex flex-col space-y-4">
-                  <Typography className="flex items-center space-x-2 py-2 border-mountain-200 border-b font-medium text-indigo-900">
+                  <Typography className="border-mountain-200 flex items-center space-x-2 border-b py-2 font-medium text-indigo-900">
                     <span className="mr-2">📅</span> Post Scheduling
                   </Typography>
                   <PostScheduleEditor
--- src/features/media-automation/scheduling/routes/AutoScheduling.tsx
+++ src/features/media-automation/scheduling/routes/AutoScheduling.tsx
@@ -1,47 +1,53 @@
-import { useState, useEffect } from 'react';
-import { Calendar, dateFnsLocalizer, Event } from 'react-big-calendar';
 import { format } from 'date-fns';
-import { parse } from 'date-fns/parse';
-import { startOfWeek } from 'date-fns/startOfWeek';
 import { getDay } from 'date-fns/getDay';
 import { enUS } from 'date-fns/locale/en-US';
+import { parse } from 'date-fns/parse';
+import { startOfWeek } from 'date-fns/startOfWeek';
+import { useEffect, useState } from 'react';
+import { Calendar, dateFnsLocalizer, Event } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
-import { transformDataToEvents } from '../utils/TransformDataToEvent';
 import CustomEvent from '../components/CustomEvent';
-import { CustomDateMonthHeader, CustomDateHeader as CustomMonthHeader } from '../components/CustomMonthHeader';
+import {
+  CustomDateMonthHeader,
+  CustomDateHeader as CustomMonthHeader,
+} from '../components/CustomMonthHeader';
 import { CustomDateHeader as CustomWeekHeader } from '../components/CustomWeekHeader';
-import '../style.css'
+import '../style.css';
+import { Post, PostStatus } from '../types/PostTypes';
+import { transformDataToEvents } from '../utils/TransformDataToEvent';
 
 // Mock data based on your provided structure
 const mockProjects = [
   {
-    "id": 12,
-    "title": "test test",
-    "status": "ACTIVE",
-    "platform": {
-      "id": 16,
-      "name": "FACEBOOK"
+    id: 12,
+    title: 'test test',
+    status: 'ACTIVE',
+    platform: {
+      id: 16,
+      name: 'FACEBOOK',
     },
-    "postCount": 2,
-    "nextPostAt": null
-  }
+    postCount: 2,
+    nextPostAt: null,
+  },
 ];
 
-const mockPosts = [
+const mockPosts: Post[] = [
   {
-    "id": 13,
-    "autoProjectId": 12,
-    "content": "In your free time, do you prefer to enjoy some solitude or to hang out with friends? Both options have their own charm...",
-    "scheduledAt": "2025-07-21T10:00:00.000Z", // Example future date
-    "status": "PENDING"
+    id: 13,
+    autoProjectId: 12,
+    content:
+      'In your free time, do you prefer to enjoy some solitude or to hang out with friends? Both options have their own charm...',
+    scheduledAt: '2025-07-21T10:00:00.000Z', // Example future date
+    status: PostStatus.PENDING,
   },
   {
-    "id": 12,
-    "autoProjectId": 12,
-    "content": "There's something truly comforting about sleep—it's where we continue to exist beyond the chaos of the day...",
-    "scheduledAt": "2025-07-19T13:00:00.000Z",
-    "status": "POSTED"
-  }
+    id: 12,
+    autoProjectId: 12,
+    content:
+      "There's something truly comforting about sleep—it's where we continue to exist beyond the chaos of the day...",
+    scheduledAt: '2025-07-19T13:00:00.000Z',
+    status: PostStatus.POSTED,
+  },
 ];
 
 const locales = {
@@ -65,13 +71,13 @@ const AutoScheduling = () => {
   }, []);
 
   return (
-    <div className="flex gap-2 bg-white p-4 border-mountain-200 rounded-t-3xl w-full h-[calc(100vh-4rem)] overflow-y-auto text-sm">
+    <div className="border-mountain-200 flex h-[calc(100vh-4rem)] w-full gap-2 overflow-y-auto rounded-t-3xl bg-white p-4 text-sm">
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
-        className='rounded-lg w-full h-full custom-scrollbar'
+        className="custom-scrollbar h-full w-full rounded-lg"
         tooltipAccessor={null}
         components={{
           month: {
@@ -84,13 +90,14 @@ const AutoScheduling = () => {
           event: CustomEvent,
         }}
         eventPropGetter={() => {
-          let backgroundColor = 'white';
+          const backgroundColor = 'white';
           return {
             style: {
               backgroundColor,
               color: 'black',
               borderRadius: '0.5rem',
-              boxShadow: '0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.06)',
+              boxShadow:
+                '0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.06)',
               padding: '2px 4px',
               border: '1px solid #d1d1d1',
             },
