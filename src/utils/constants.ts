5a93833c71c4face8cd33b57220350a7ead63ada
nmthong226 <nmthong226@gmail.com>
2025-07-30 11:57:17 +0700

    fix constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -9,6 +9,7 @@ export const dashboardBG =
 export const featuresShowcase = [
   {
     label: 'Text To Image',
+    description: 'Create art from text using advanced AI.',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/nxyormqdp4kggz0ncymu?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
@@ -20,26 +21,33 @@ export const featuresShowcase = [
   // },
   {
     label: 'Image Editor',
+    description: 'A powerful editor for perfect visuals.',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/rtbrxgmxcgkz9evmljks?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
   {
     label: 'Text Editor',
+    description: 'Write, edit, and enhance with AI.',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/ztwlihdk1mqirwi1pzoc?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
   {
     label: 'Content Approval Workflow',
+    description:
+      'Approve automation pushing content to other social platforms.',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/uxeuuht93gl7mbtp1f3r?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
   {
     label: 'Automatic Content Creator',
+    description:
+      'Prepare the ideas for bots to create and upload content row by row',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/ydd0gojvbdnqejqifk2o?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
   {
     label: 'Scheduled Content Posting',
+    description: 'Schedule when to publish the content automatically.',
     url: 'https://res.cloudinary.com/dqxtf297o/image/upload/f_auto,q_auto/v1/artshare-asset/dashboard-feat-example/leuamzwred3navfdhc6v?blur=300&q=1',
     destination: '/image/tool/text-to-image',
   },
@@ -68,7 +76,6 @@ export const routesForHeaders: HeaderRoute[] = [
   {
     path: '/dashboard/updates',
     label: 'App Updates',
-    description: '',
   },
   {
     path: '/explore',
@@ -131,7 +138,6 @@ export const routesForHeaders: HeaderRoute[] = [
   {
     path: '/auto/scheduling',
     label: 'Project Scheduling',
-    description: '',
   },
   {
     path: '/auto/projects',
