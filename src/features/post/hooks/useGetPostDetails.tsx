ea482f20455094c12e623f162e4db66dc0761337
quyhoaphantruong <quyhoaphantruong@gmail.com>
2025-08-09 15:03:02 +0700

    perf get post details for view

--- src/features/post/hooks/useGetPostDetails.tsx
+++ src/features/post/hooks/useGetPostDetails.tsx
@@ -1,6 +1,6 @@
 import { postKeys } from '@/lib/react-query/query-keys';
 import { useQuery } from '@tanstack/react-query';
-import { fetchPost } from '../api/post.api';
+import { fetchPost, fetchPostForView } from '../api/post.api';
 
 export const useGetPostDetails = (postId?: string | number) => {
   const numericPostId = postId ? Number(postId) : NaN;
@@ -12,6 +12,23 @@ export const useGetPostDetails = (postId?: string | number) => {
 
     queryFn: async () => {
       const response = await fetchPost(numericPostId);
+      return response.data;
+    },
+    enabled: isEnabled,
+    staleTime: 5 * 60 * 1000,
+  });
+};
+
+export const useGetPostDetailsForView = (postId?: string | number) => {
+  const numericPostId = postId ? Number(postId) : NaN;
+  // The `enabled` flag prevents this from running with an invalid ID
+  const isEnabled = !!postId && !isNaN(numericPostId);
+
+  return useQuery({
+    queryKey: postKeys.details(numericPostId),
+
+    queryFn: async () => {
+      const response = await fetchPostForView(numericPostId);
       return response.data;
     },
     enabled: isEnabled,
