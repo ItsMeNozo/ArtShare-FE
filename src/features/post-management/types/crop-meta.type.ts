48271e13547b841940b7bc2c985f7d29fa458bf5
liwjln <103946556+liwjln@users.noreply.github.com>
2025-08-12 14:15:32 +0700

    [Tu] Fix: add auto post navigation confirmation dialog, refactor gallery view (#183)
    * fix: add confirmation dialog for auto post navigation
    
    * feat: refactor gallery view

--- src/features/post-management/types/crop-meta.type.ts
+++ src/features/post-management/types/crop-meta.type.ts
@@ -6,4 +6,6 @@ export interface ThumbnailMeta {
   aspect: number | undefined; // naming should've been selectedAspectValue For react-easy-crop's aspect prop
   selectedAspect: string; // naming should've been selectedAspectLabel, For your UI buttons e.g., "1:1", "Free"
   croppedAreaPixels: Area | null; // This is the area to be cropped, used in getCroppedImg function
+  width?: number;
+  height?: number;
 }
