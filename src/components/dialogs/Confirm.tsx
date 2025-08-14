e687caea76a097d350ae68e8c4fa94522811db51
liwjln <phamtrantuantu17@gmail.com>
2025-08-14 16:05:19 +0700

    fix: confirmation dialog and plan desc

--- src/components/dialogs/Confirm.tsx
+++ src/components/dialogs/Confirm.tsx
@@ -32,14 +32,16 @@ export default function ConfirmDialog({
 }: ConfirmDialogProps) {
   return (
     <Dialog open={open} onOpenChange={onCancel}>
-      <DialogContent className="flex flex-col border-mountain-200 w-108">
+      <DialogContent className="border-mountain-200 flex w-108 flex-col">
         <DialogHeader>
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
-        <div className="flex justify-center items-center bg-mountain-50 py-6">
-          {icon}
-        </div>
+        {icon && (
+          <div className="bg-mountain-50 flex items-center justify-center py-6">
+            {icon}
+          </div>
+        )}
         <DialogFooter>
           <Button
             className="bg-mountain-200 hover:bg-mountain-200/80 text-mountain-950"
@@ -48,7 +50,7 @@ export default function ConfirmDialog({
             Cancel
           </Button>
           <Button
-            className="bg-red-700 hover:bg-red-700/80 text-mountain-50"
+            className="text-mountain-50 bg-red-700 hover:bg-red-700/80"
             onClick={onConfirm}
             disabled={isLoading}
           >
